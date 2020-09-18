f8624b0f37983839b4526656a46e24cbcc0bef09
Fangzhou Liu <fangzhouliu@FangzhoudeMacBook-Air.local>
2020-09-18 15:03:09 -0700

    Add something

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var Comment = require("./models/comment");
 var User = require("./models/user");
 var seedDB = require("./seeds");
 
+// requiring routes
+var commentRoutes = require("./routes/comments")
+var campgroundRoutes = require("./routes/campgrounds")
+var indexRoutes = require("./routes/index")
+
 
 // var campgrounds = [
 //     {name: "Tiger", image: "https://scx1.b-cdn.net/csz/news/800/2019/tiger.jpg"},
@@ -22,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 console.log(__dirname);
-seedDB();
+// seedDB();
 
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog",
@@ -30,129 +35,22 @@ app.use(require("express-session")({
     saveUninitialized: false
 
 }));
+
+// Passport Configuration
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-// schema set up
-
-
-// Campground.create({
-//     name : "Tiger",
-//     image: "https://scx1.b-cdn.net/csz/news/800/2019/tiger.jpg",
-//     description: "Cute tiger"
-// }, function(err, campground) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("NEWLY CREATE CAMPGROUND");
-//         console.log(campground);
-//     }
-// });
-
-
-app.get("/", function(req, res) {
-    res.render("landing");
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
 });
 
-
-app.get("/campgrounds", function(req, res){
-    Campground.find({}, function(err, allCampgrounds){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/index", {campgrounds : allCampgrounds});
-        }
-    });
-    //res.render("campgrounds",{campgrounds: campgrounds});
-});
-
-app.post("/campgrounds", function(req, res) {    
-    // Get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc}
-    // campgrounds.push(newCampground);
-    // Create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if (err) {
-            console.log(err);
-        } else {
-            // Redirect to campgrounds page
-            res.redirect("./campgrounds");
-        }
-    });
-});
-
-app.get("/campgrounds/new", function(req, res) {
-    res.render("campgrounds/new");
-});
-
-app.get("/campgrounds/:id", function(req, res){
-    // find the campground with id
-    //
-    //res.send("this will be the show page");
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/show", {campground : foundCampground});
-        }
-    });
-    
-});
-
-app.get("/campgrounds/:id/comments/new", function(req, res){
-    //find campground by id
-    Campground.findById(req.params.id, function(err, campground) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("comments/new", {campground: campground});
-        }
-    });
-});
-
-app.post("/campgrounds/:id/comments", function(req, res){
-    // lookup campground using id
-    Campground.findById(req.params.id, function(err, campground){
-        if (err) {
-            console.log(err);
-            res.redirect("/campgrounds");
-        } else {
-            Comment.create(req.body.comment, function(err, comment){
-                if (err) {
-                    console.log(err);
-                } else {    
-                    campground.comments.push(comment);
-                    campground.save();
-                    res.redirect('/campgrounds/' + campground._id);
-                }
-            })
-        }
-    })
-})
-
-//show the register form
-app.get("/register", function(req, res){
-    res.render("register");
-})
-app.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if (err) {
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/campgrounds")
-        });
-    });
-});
+app.use(indexRoutes);
+app.use(campgroundRoutes);
+app.use(commentRoutes);
 
 // Starting listening for request
 app.listen(3000, function() {
--- seeds.js
+++ seeds.js
@@ -25,33 +25,33 @@ var data = [
 function seedDB() {
     // Remove all
     Campground.remove({}, function(err){
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("remove campgrounds");
-        }
-        data.forEach(function(seed){
-            Campground.create(seed, function(err, campground) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log("added a campground")
-                    Comment.create(
-                        {
-                            text : "this place is nice",
-                            author: "Homer"
-                        }, function (err, comment) {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                campground.comments.push(comment);
-                                campground.save();
-                                console.log("Created new comment");
-                            }
-                        });
-                }
-            });
-        });
+        // if (err) {
+        //     console.log(err);
+        // } else {
+        //     console.log("remove campgrounds");
+        // }
+        // data.forEach(function(seed){
+        //     Campground.create(seed, function(err, campground) {
+        //         if (err) {
+        //             console.log(err)
+        //         } else {
+        //             console.log("added a campground")
+        //             Comment.create(
+        //                 {
+        //                     text : "this place is nice",
+        //                     author: "Homer"
+        //                 }, function (err, comment) {
+        //                     if (err) {
+        //                         console.log(err);
+        //                     } else {
+        //                         campground.comments.push(comment);
+        //                         campground.save();
+        //                         console.log("Created new comment");
+        //                     }
+        //                 });
+        //         }
+        //     });
+        // });
     });
     
 
